f198c47625ded5bf9bb18cd3b268e965b706d432
Ludvig Rydahl <ludvig.rydahl@xlent.se>
2024-04-15 00:23:43 +0200

    feat: adds nvimtree

---
+++ lua/plugins/nvimtree.lua
@@ -0,0 +1,18 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+return {
+	"nvim-tree/nvim-tree.lua",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		require("nvim-tree").setup({
+			actions = {
+				open_file = {
+					quit_on_open = true,
+				},
+			},
+		})
+	end,
+}
