15745d247eeefd367d27968b22253e0e3894b782
Ludvig Rydahl <ludvig.rydahl@xlent.se>
2024-04-17 01:03:43 +0200

    feat: Adds snippets

--- init.lua
+++ init.lua
@@ -3,7 +3,6 @@ require("config.options")
 require("config.keymaps")
 require("config.autocmd")
 
-require("luasnip.loaders.from_vscode").lazy_load()
 require("lspconfig").tsserver.setup({
 	on_attach = function(client, bufnr)
 		local ns = vim.lsp.diagnostic.get_namespace(client.id)
